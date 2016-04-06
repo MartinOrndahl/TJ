96a2b8343f428647a91a2149ff0b06a051242c85
Martin Örndahl <martin.orndahl@gmail.com>
2016-04-06 15:03:35 +0200

    Försöka läsa från picturs

--- app/src/main/java/com/beatem/tj/GalleryFragment.java
+++ app/src/main/java/com/beatem/tj/GalleryFragment.java
@@ -2,10 +2,13 @@ package com.beatem.tj;
 
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.File;
 
 
 /**
@@ -13,6 +16,8 @@ import android.view.ViewGroup;
  */
 public class GalleryFragment extends Fragment {
 
+    static final String MEDIA_DIRECTORY = "TravelJournal";
+    private static final int READ_REQUEST_CODE = 42;
 
     public GalleryFragment() {
         // Required empty public constructor
@@ -24,6 +29,10 @@ public class GalleryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+
+    private void readPictureDirectory(){
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
     }
 
 }
