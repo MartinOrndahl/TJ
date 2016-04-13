e375307b4a78b48d0f2e821b2b1635cca9049d42
Martin Örndahl <martin.orndahl@gmail.com>
2016-04-13 11:21:12 +0200

    Fixat galleryFragment

--- app/src/main/java/com/beatem/tj/GalleryFragment.java
+++ app/src/main/java/com/beatem/tj/GalleryFragment.java
@@ -1,6 +1,7 @@
 package com.beatem.tj;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
 import android.net.Uri;
@@ -11,8 +12,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -23,9 +28,8 @@ import java.util.ArrayList;
  */
 public class GalleryFragment extends Fragment {
 
-    private static int RESULT_LOAD_IMAGE = 1;
     private static GridView gridView;
-    private int[]image={R.drawable.vector_drawable_ic_map_black___px, R.drawable.vector_drawable_ic_cancel_black___px};
+    private int[]imageIDs={R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher};
 
     public GalleryFragment() {
         // Required empty public constructor
@@ -36,25 +40,62 @@ public class GalleryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
-
         gridView = (GridView) view.findViewById(R.id.view_gallery);
-        ImageAdapter adapter = new ImageAdapter(getContext(), getImages());
-
-        gridView.setAdapter(adapter);
+        gridView.setAdapter(new ImageAdapter(getContext()));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            public void onItemClick(AdapterView<?> parent,
+                                    View v, int position, long id)
+            {
+                Toast.makeText(getContext(),
+                        "pic" + (position + 1) + " selected",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
         // Inflate the layout for this fragment
         return view;
     }
 
+    public class ImageAdapter extends BaseAdapter
+    {
+        private Context context;
 
-    private ArrayList<Images> getImages(){
-        ArrayList<Images> images = new ArrayList<>();
-        images.add(new Images(image[0]));
-        images.add(new Images(image[1]));
+        public ImageAdapter(Context c)
+        {
+            context = c;
+        }
 
+        //---returns the number of images---
+        public int getCount() {
+            return imageIDs.length;
+        }
 
-        return images;
+        //---returns the ID of an item---
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        //---returns an ImageView view---
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            ImageView imageView;
+            if (convertView == null) {
+                imageView = new ImageView(context);
+                imageView.setLayoutParams(new GridView.LayoutParams(185, 185));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(5, 5, 5, 5);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setImageResource(imageIDs[position]);
+            return imageView;
+        }
     }
 
 }
