fdb66a9316adf35092d3132e9f4bf1f97e466c64
JoelBuhrman <dic13jbu@student.lu.se>
2016-06-09 13:13:18 +0200

    Fått in allt från kameraaktiviteten och fått bort alla dess errors. Nästa steg är att koppla ihop det med appen

---
+++ app/src/main/java/com/beatem/tj/OpenCamera/Preview/CameraSurface/CameraSurface.java
@@ -0,0 +1,17 @@
+package com.beatem.tj.OpenCamera.Preview.CameraSurface;
+
+import android.graphics.Matrix;
+import android.media.MediaRecorder;
+import android.view.View;
+
+import com.beatem.tj.OpenCamera.CameraController.CameraController;
+
+/** Provides support for the surface used for the preview - this can either be
+ *  a SurfaceView or a TextureView.
+ */
+public interface CameraSurface {
+	abstract View getView();
+	abstract void setPreviewDisplay(CameraController camera_controller); // n.b., uses double-dispatch similar to Visitor pattern - behaviour depends on type of CameraSurface and CameraController
+	abstract void setVideoRecorder(MediaRecorder video_recorder);
+	abstract void setTransform(Matrix matrix);
+}
---
+++ app/src/main/java/com/beatem/tj/OpenCamera/ToastBoxer.java
@@ -0,0 +1,12 @@
+package com.beatem.tj.OpenCamera;
+
+import android.widget.Toast;
+
+/** Allows methods to update a Toast with a new Toast.
+ */
+public class ToastBoxer {
+	public Toast toast = null;
+
+	public ToastBoxer() {
+	}
+}
